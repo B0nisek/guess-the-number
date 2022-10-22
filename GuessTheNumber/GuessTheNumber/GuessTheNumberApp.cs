eae9d40d8033d1c0bc1e7d4770b401c87d1a7eed
Kristian Mateka <kristian.mateka@gmail.com>
2022-10-22 10:29:24 +0200

    solution

--- GuessTheNumber/GuessTheNumber/GuessTheNumberApp.cs
+++ GuessTheNumber/GuessTheNumber/GuessTheNumberApp.cs
@@ -1,18 +1,36 @@
-﻿using System;
-
-namespace GuessTheNumber;
+﻿namespace GuessTheNumber;
 public static class GuessTheNumberApp
 {
     public static int Run(int upperLimit)
     {
-        var result = 1;
-        var isNumberCorrect = false;
+        var numberOfTakes = 0;
+        var lowerLimit = 1;
 
-        while (isNumberCorrect == false) 
+        while (true) 
         {
-            isNumberCorrect = true;
+            numberOfTakes++;
+
+            var numberGuess = (lowerLimit + upperLimit) / 2;
+
+            Console.WriteLine($"Is {numberGuess} your number? - less than, = correct, + greater than");
+            var response = Console.ReadKey().KeyChar;
+
+            if (response == '=')
+            {
+                break;
+            }
+
+            if (response == '+')
+            {
+                lowerLimit = numberGuess;
+            }
+
+            if (response == '-')
+            {
+                upperLimit = numberGuess;
+            }
         }
 
-        return result;
+        return numberOfTakes;
     }
 }
